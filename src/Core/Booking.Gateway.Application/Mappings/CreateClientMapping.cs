65f6170da9cde263fb4364f9d46bd7ea7a4da67a
Ann Dyadkova <a.dyadkova@angarasecurity.ru>
2024-02-07 21:59:14 +0500

    Добавила MassTransit

---
+++ src/Core/Booking.Gateway.Application/Mappings/CreateClientMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Booking.Gateway.Application.Features.ClientFeatures.CreateClient;
+using Otus.Booking.Common.Booking.Contracts.Authentication.Requests;
+using Otus.Booking.Common.Booking.Contracts.Authentication.Responses;
+
+namespace Booking.Gateway.Application.Mappings;
+
+public sealed class CreateClientMapper : Profile
+{
+    public CreateClientMapper()
+    {
+        CreateMap<CreateClientRequest, CreateClient>();
+        CreateMap<CreateClientResult, CreateClientResponse>();
+    }
+}
