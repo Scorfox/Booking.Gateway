7e3b9078d28823501096af74d63d3420eef9564a
Haxend <dunaevskyilya@gmail.com>
2024-03-01 13:56:53 +0300

    refactor: GetUserId

--- src/Core/Booking.Gateway.Application/Features/ClientFeatures/GetClient/GetClientHandler.cs
+++ src/Core/Booking.Gateway.Application/Features/ClientFeatures/GetClient/GetClientHandler.cs
@@ -2,6 +2,7 @@
 using Booking.Gateway.Application.Features.ClientFeatures.CreateClient;
 using MassTransit;
 using MediatR;
+using Otus.Booking.Common.Booking.Contracts.User.Models;
 using Otus.Booking.Common.Booking.Contracts.User.Responses;
 using ContractRequests = Otus.Booking.Common.Booking.Contracts.User.Requests;
 
@@ -20,7 +21,7 @@ public sealed class GetClientHandler : IRequestHandler<GetClientRequest, GetClie
 
     public async Task<GetClientResponse> Handle(GetClientRequest request, CancellationToken cancellationToken)
     {
-        var response = await _requestUser.GetResponse<GetUserResult>(_mapper.Map<ContractRequests.GetUserId>(request));
+        var response = await _requestUser.GetResponse<FullUserDto>(_mapper.Map<ContractRequests.GetUserId>(request));
         return _mapper.Map<GetClientResponse>(response.Message);
     }
 }
