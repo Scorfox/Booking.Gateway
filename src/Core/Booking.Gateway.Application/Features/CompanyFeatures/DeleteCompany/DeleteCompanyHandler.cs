40bf5b3ca63324c07c4c010bb86794c8ef51536b
Ann Dyadkova <a.dyadkova@angarasecurity.ru>
2024-03-02 14:08:30 +0500

    feat: Добавила маппинг Exception в корректный ответ
    404 - не найден, 400 - bad request

--- src/Core/Booking.Gateway.Application/Features/CompanyFeatures/DeleteCompany/DeleteCompanyHandler.cs
+++ src/Core/Booking.Gateway.Application/Features/CompanyFeatures/DeleteCompany/DeleteCompanyHandler.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using MassTransit;
 using MediatR;
+using Otus.Booking.Common.Booking.Contracts.Company.Responses;
 using ContractRequests = Otus.Booking.Common.Booking.Contracts.Company.Requests;
 
 namespace Booking.Gateway.Application.Features.CompanyFeatures.DeleteCompany;
@@ -18,7 +19,7 @@ public sealed class DeleteCompanyHandler : IRequestHandler<DeleteCompanyRequest,
 
     public async Task<DeleteCompanyResponse> Handle(DeleteCompanyRequest request, CancellationToken cancellationToken)
     {
-        await _requestCompany.GetResponse<ContractRequests.DeleteCompany>(
+        await _requestCompany.GetResponse<DeleteCompanyResult>(
             _mapper.Map<ContractRequests.DeleteCompany>(request), cancellationToken);
 
         return new DeleteCompanyResponse{Id = request.Id};
